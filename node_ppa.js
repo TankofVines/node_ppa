4013253a689f591b789cb1ed3511fc63a0fc1d67
kas673 <kevin.stofan@gmail.com>
2012-08-21 01:07:22 -0400

    Added the distance calculation and the response...moved a bunch of stuff around...cleaned up some var names

--- node_ppa.js
+++ node_ppa.js
@@ -5,58 +5,42 @@ var sylvester = require('sylvester');
 var async = require('async');
 
 function nndist(query, res, callback) {
-    coords = JSON.parse(query.coordinates);
-    distance = [];
+    var coords = JSON.parse(query.coordinates);
+    var darray = [];
+    var nnvalue = 0;
     
+    // // Synchronous Javascript version of the distance array constructor for reference
+    // // Could be moved over to client-side if needed
     // for (i=0; i<coords.length; i++) {
-        // distance.push([]);
+        // darray.push([]);
         // for (j=0; j<coords.length; j++) {
 //             
             // // Distance formula
-            // currentDistance = Math.sqrt( Math.pow( coords[j][0] - coords[i][0], 2) + Math.pow( coords[j][1] - coords[i][1], 2) )
+            // dist = Math.sqrt( Math.pow( coords[j][0] - coords[i][0], 2) + Math.pow( coords[j][1] - coords[i][1], 2) )
 //             
             // // Seems that the verdict is still out on what is faster
             // // In V8 it doesn't seem to matter, but some preference to push()
-            // distance[i].push(currentDistance);
+            // darray[i].push(dist);
         // }
     // }
     
-    function rowBuilder(i) {
-        if(i<coords.length) {
-            distance.push([]);
-            function distCalc(j) {
-                if(j<coords.length) {
-                    // Distance formula
-                    currentDistance = Math.sqrt( Math.pow( coords[j][0] - coords[i][0], 2) + Math.pow( coords[j][1] - coords[i][1], 2) );
-                    
-                    // Seems that the verdict is still out on what is faster
-                    // In V8 it doesn't seem to matter, but some preference to push()
-                    distance[i].push(currentDistance);
-                    distCalc(j+1);
-                } else {
-                    rowBuilder(i+1);
-                }
-            }
-            distCalc(0);
-        }
-    }
-    rowBuilder(0);
-    
+    // async package version using forEachSeries for serial flow control
+    // To do: Test speed difference between this and recursive functions
     // async.forEachSeries(coords, function(coord, callback) {
         // row = [];
         // async.forEachSeries(coords, function(neighbor, cb) {
             // // Distance formula
-            // currentDistance = Math.sqrt( Math.pow( neighbor[0] - coord[0], 2) + Math.pow( neighbor[1] - coord[1], 2) );
+            // dist = Math.sqrt( Math.pow( neighbor[0] - coord[0], 2) + Math.pow( neighbor[1] - coord[1], 2) );
             // // Seems that the verdict is still out on what is faster
             // // In V8 it doesn't seem to matter, but some preference to push()
-            // row.push(currentDistance);
+            // row.push(dist);
             // cb();
         // }, function(err) {
             // if (err) {
                 // console.log(err);
             // }
         // });
-        // distance.push(row);
+        // darray.push(row);
         // callback();
         // }, function(err) {
             // if (err) {
@@ -64,7 +48,57 @@ function nndist(query, res, callback) {
             // }
     // });
     
-    console.log(distance);
+    function rowBuilder(i) {
+        if(i<coords.length) {
+            darray.push([]);
+            function distCalc(j) {
+                if(j<coords.length) {
+                    // Distance formula
+                    dist = Math.sqrt( Math.pow( coords[j][0] - coords[i][0], 2) + Math.pow( coords[j][1] - coords[i][1], 2) );
+                    
+                    // Seems that the verdict is still out on what is faster
+                    // In V8 it doesn't seem to matter, but some preference to push()
+                    darray[i].push(dist);
+                    distCalc(j+1);
+                } else {
+                    rowBuilder(i+1);
+                }
+            }
+            distCalc(0);
+        } else {
+            var nnarray = [];
+        
+            function rowSorter(i) {
+                if(i<darray.length) {
+                    // Array.min = function( array ){
+                        // return Math.min.apply( Math, array );
+                    // };
+                    ascendingrow = darray.sort(function(a,b){return a - b});
+                    nndist = parseFloat(ascendingrow[1]);
+                    nnarray.push(nndist);
+                    rowSorter(i+1);
+                } else {
+                    var nndistsum = 0;
+                    function meanDist(j) {
+                        if(j<nnarray.length) {
+                            nndistsum += nnarray[j];
+                            meanDist(j+1);
+                        } else {
+                            nnvalue = nndistsum / nnarray.length;
+                        }
+                    }
+                    meanDist(0);
+                }
+            }
+            rowSorter(0);
+        }
+    }
+    rowBuilder(0);
+    
+    var responseobjstring = JSON.stringify(nnvalue);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(callback + '(' + responseobjstring + ')');
+    
 }
 
 var server = http.createServer(function(req, res) {
