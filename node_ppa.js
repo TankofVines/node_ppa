f0f530a4e69212d617888e2ac71d7eaffee479a8
kas673 <kevin.stofan@gmail.com>
2012-09-26 11:57:54 -0400

    Commented out the recursive version of the nndist and finished off the asyn package version

--- node_ppa.js
+++ node_ppa.js
@@ -28,84 +28,105 @@ function nndist(query, res, callback) {
     
     // async package version using forEachSeries for serial flow control
     // To do: Test speed difference between this and recursive functions
-    // async.forEachSeries(coords, function(coord, callback) {
-        // row = [];
-        // async.forEachSeries(coords, function(neighbor, cb) {
-            // // Distance formula
-            // dist = Math.sqrt( Math.pow( neighbor[0] - coord[0], 2) + Math.pow( neighbor[1] - coord[1], 2) );
-            // // Seems that the verdict is still out on what is faster
-            // // In V8 it doesn't seem to matter, but some preference to push()
-            // row.push(dist);
-            // cb();
-        // }, function(err) {
-            // if (err) {
-                // console.log(err);
-            // }
-        // });
-        // darray.push(row);
-        // callback();
-        // }, function(err) {
-            // if (err) {
-                // console.log(err);
-            // }
-    // });
+    async.forEachSeries(coords, function(coord, callback) {
+        row = [];
+        async.forEachSeries(coords, function(neighbor, cb) {
+            // Distance formula
+            dist = Math.sqrt( Math.pow( neighbor[0] - coord[0], 2) + Math.pow( neighbor[1] - coord[1], 2) );
+            // Seems that the verdict is still out on what is faster
+            // In V8 it doesn't seem to matter, but some preference to push()
+            row.push(dist);
+            cb();
+        }, function(err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+        darray.push(row);
+        callback();
+        }, function(err) {
+            if (err) {
+                console.log(err);
+            }
+        var nnarray = [];    
+        async.forEachSeries(darray, function(distance, callback) {
+            ascendingrow = darray.sort(function(a,b){return a - b});
+            currentdist = parseFloat(ascendingrow[1]);
+            nnarray.push(currentdist);
+            callback();
+        }, function(err) {
+            if (err) {
+                console.log(err);
+            }
+            var nndistsum = 0;
+            async.forEachSeries(nnarray, function(nneighbor, cb) {
+                nndistsum += nneighbor;
+                cb();
+            }, function(err) {
+                if (err) {
+                    console.log(err);
+                }
+                nnvalue = Math.round(nndistsum / nnarray.length);
+            });
+        });
+    });
     
     // The row builder function creates a new row of values in the D matrix
     // It is called recursively until the coordinates array is traversed
     // Once all the coordinates have been iterated over a new array is used
     // to 
-    function rowBuilder(i) {
-        if(i<coords.length) {
-            darray.push([]);
-            
-            // The distance calculation function calculates the distance between the current point/row (i)
-            // and every other point (j) then pushed that value into the current row and traverses the row recursively
-            function distCalc(j) {
-                if(j<coords.length) {
-                    // Distance formula
-                    dist = Math.sqrt( Math.pow( coords[j][0] - coords[i][0], 2) + Math.pow( coords[j][1] - coords[i][1], 2) );
-                    
-                    // Seems that the verdict is still out on what is faster
-                    // In V8 it doesn't seem to matter, but some preference to push()
-                    darray[i].push(dist);
-                    distCalc(j+1);
-                } else {
-                    rowBuilder(i+1);
-                }
-            }
-            distCalc(0);
-        } else {
-            var nnarray = [];
-            
-            // The row sorter function iterates of the D matrix rows and sorts them in ascending order
-            function rowSorter(i) {
-                if(i<darray.length) {
-                    // Array.min = function( array ){
-                        // return Math.min.apply( Math, array );
-                    // };
-                    ascendingrow = darray.sort(function(a,b){return a - b});
-                    currentdist = parseFloat(ascendingrow[1]);
-                    nnarray.push(currentdist);
-                    rowSorter(i+1);
-                } else {
-                    var nndistsum = 0;
-                    
-                    // Iterates over the sorted rows and adds the value of shortest distance to the running total
-                    function meanDist(j) {
-                        if(j<nnarray.length) {
-                            nndistsum += nnarray[j];
-                            meanDist(j+1);
-                        } else {
-                            nnvalue = Math.round(nndistsum / nnarray.length);
-                        }
-                    }
-                    meanDist(0);
-                }
-            }
-            rowSorter(0);
-        }
-    }
-    rowBuilder(0);
+    // function rowBuilder(i) {
+        // if(i<coords.length) {
+            // darray.push([]);
+//             
+            // // The distance calculation function calculates the distance between the current point/row (i)
+            // // and every other point (j) then pushed that value into the current row and traverses the row recursively
+            // function distCalc(j) {
+                // if(j<coords.length) {
+                    // // Distance formula
+                    // dist = Math.sqrt( Math.pow( coords[j][0] - coords[i][0], 2) + Math.pow( coords[j][1] - coords[i][1], 2) );
+//                     
+                    // // Seems that the verdict is still out on what is faster
+                    // // In V8 it doesn't seem to matter, but some preference to push()
+                    // darray[i].push(dist);
+                    // distCalc(j+1);
+                // } else {
+                    // rowBuilder(i+1);
+                // }
+            // }
+            // distCalc(0);
+        // } else {
+            // var nnarray = [];
+//             
+            // // The row sorter function iterates of the D matrix rows and sorts them in ascending order
+            // function rowSorter(i) {
+                // if(i<darray.length) {
+                    // // Array.min = function( array ){
+                        // // return Math.min.apply( Math, array );
+                    // // };
+                    // ascendingrow = darray.sort(function(a,b){return a - b});
+                    // currentdist = parseFloat(ascendingrow[1]);
+                    // nnarray.push(currentdist);
+                    // rowSorter(i+1);
+                // } else {
+                    // var nndistsum = 0;
+//                     
+                    // // Iterates over the sorted rows and adds the value of shortest distance to the running total
+                    // function meanDist(j) {
+                        // if(j<nnarray.length) {
+                            // nndistsum += nnarray[j];
+                            // meanDist(j+1);
+                        // } else {
+                            // nnvalue = Math.round(nndistsum / nnarray.length);
+                        // }
+                    // }
+                    // meanDist(0);
+                // }
+            // }
+            // rowSorter(0);
+        // }
+    // }
+    // rowBuilder(0);
     
     // Respond with the stringified nearest neighbor value
     var responseobjstring = JSON.stringify(nnvalue);
